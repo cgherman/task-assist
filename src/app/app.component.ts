6380a04e21f53bfcdf0de5bc212770d58cb9e83b
cgherman <cgherman@gmail.com>
2018-07-05 11:45:54 -0500

    stable data access within Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,52 +10,90 @@ declare var gapi: any;
 
 export class AppComponent {
   title = 'TaskAssist';
-  signedInFlag = false;
+  public auth2:any
+  static client_id = "782561556087-8vrgbd6393gagmenk100qmv4lfbpulrg.apps.googleusercontent.com";
+  static scope = "https://www.googleapis.com/auth/tasks";
+  static discoveryDocs = "https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest";
+  static api_key = "AIzaSyBke1n7BqFee0XcM7_WbIg337YsPrROgh0";
 
   constructor(private zone: NgZone, private meta: Meta) {
-    this.meta.addTag({ name: 'google-signin-client_id', content: '782561556087-8vrgbd6393gagmenk100qmv4lfbpulrg.apps.googleusercontent.com' });
-    this.meta.addTag({ name: 'google-signin-scope', content: 'https://www.googleapis.com/auth/tasks' });
+    this.meta.addTag({ name: 'google-signin-client_id', content: AppComponent.client_id });
+    this.meta.addTag({ name: 'google-signin-scope', content: AppComponent.scope });
   }
 
   ngAfterViewInit() {
-    gapi.signin2.render('googleSigninButton', {
-        'onsuccess': param => this.onSignIn(param),
-        'scope': 'https://www.googleapis.com/auth/tasks',
-        'longtitle': true,
-        'theme': 'light',
-        //TODO: 'onfailure': ...
-      })
+    gapi.load('auth2',  () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: AppComponent.client_id,
+        scope: AppComponent.scope
+      });
+      this.attachSignin(document.getElementById('glogin'));
+    });
+
+    gapi.load('client',  () => {
+      this.auth2 = gapi.client.load('tasks', 'v1', () => {
+        gapi.auth.authorize({ client_id: AppComponent.client_id, scope: AppComponent.scope, api_key: AppComponent.api_key, discoveryDocs: AppComponent.discoveryDocs }, authResult => {
+          if (authResult && !authResult.error) {
+
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+
+            // Handle the initial sign-in state.
+            this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+          } else {
+            console.log("Unable to connect: " + authResult.error);
+            /* TODO: handle authorization error */
+          }
+        });        
+      });
+    });
   }
 
   onSignIn(googleUser) {
-    this.zone.run(() => {
-      // Listen for sign-in state changes.
-      gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
+  }
 
-      // Handle the initial sign-in state.
-      this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-
-      // Process some test data
-      this.testApiCall();
-    });
-  };
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (loggedInUser) => {  
+        console.log( loggedInUser);
+      }, function (error) {
+        // alert(JSON.stringify(error, undefined, 2));
+      });
+  }
 
   onSignOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut();
+    gapi.auth2.getAuthInstance().signOut(
+    ).then(function(response) {
+      console.log('Successful sign-out.');
+    }, function(rejectReason) {
+      console.log('Error: ' + rejectReason.result.error.message);
+    });
+
     location.reload();
   }
 
   updateSigninStatus(isSignedIn) {
     // When signin status changes, this function is called.
     console.log('isSignedIn=' + isSignedIn);
-    this.signedInFlag = isSignedIn;
+
+    // test some data
+    if (isSignedIn) {
+      gapi.client.tasks.tasklists.list({
+      }).then(function(response) {
+        var index: number;
+        console.log('Successful sign-in.');
+        for (index = 0; index < response.result.items.length; index++) {
+          console.log(response.result.items[index].title);
+        }
+        console.log('Successful fetch of Task Lists.');
+      }, function(rejectReason) {
+        console.log('Error: ' + rejectReason.result.error.message);
+      });
+    }
   }
 
   isSignedIn() {
-    return true;
-    return this.signedInFlag;
-
     if (gapi == null || gapi.auth2 == null) {
       return false;
     }
@@ -67,19 +105,4 @@ export class AppComponent {
 
     return auth2.isSignedIn.get();
   }
-
-  testApiCall() {
-    gapi.client.tasks.tasklists.list({
-    }).then(function(response) {
-      var index: number;
-      console.log('Successful sign-in.');
-      for (index = 0; index < response.result.items.length; index++) {
-        console.log(response.result.items[index].title);
-      }
-      console.log('Successful fetch of Task Lists.');
-    }, function(rejectReason) {
-      console.log('Error: ' + rejectReason.result.error.message);
-    });
-  }
-
 }
